e5cffac1f57ac42a49d7155a6dcc5f5c6ef079e3
ChrisZhao2 <91628314+ChrisZhao2@users.noreply.github.com>
2022-01-25 11:30:21 -0800

    Update a214_simple_window1_[studentinitials].py

--- a214_simple_window1_[studentinitials].py
+++ a214_simple_window1_[studentinitials].py
@@ -8,5 +8,16 @@ root.wm_geometry("200x100")
 
 # create empty frame
 frame_login = tk.Frame(root)
-
+frame_login.grid()
+lbl_username = tk.Label(frame_login, text='Username:')
+lbl_username.pack()
+ent_username = tk.Entry(frame_login, bd=3)
+ent_username.pack(pady=5)
+tk.Label(frame_login,text="Password:",font="Courier")
 root.mainloop()
+
+#test
+def test_my_button():
+  frame_auth.tkraise()
+tk.Button(frame_login, text="LOG IN")
+command=test_my_button
